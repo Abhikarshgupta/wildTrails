c0160097f90577ebf984420df2f8ce54d9425a75
Abhikarsh Gupta <abg71297@gmail.com>
2023-05-30 19:00:27 +0530

    [PATCH] updated config for base URL and build for static deployment

--- react/vite.config.js
+++ react/vite.config.js
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/wildTrails/",
 	plugins: [react(), basicSsl()],
 	resolve: {
 		alias: {
