1716645d830e05fa268663065f0dd90d7565333e
Abhikarsh Gupta <abg71297@gmail.com>
2023-05-30 19:16:42 +0530

    [PATCH] trying different base URL for hosting

--- react/vite.config.js
+++ react/vite.config.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/wildTrails/",
+	base: "/wildTrails/react/",
 	plugins: [react(), basicSsl()],
 	resolve: {
 		alias: {
