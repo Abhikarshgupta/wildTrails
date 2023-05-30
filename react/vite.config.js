e8314669813d84554157b1c50493cb46a181033d
Abhikarsh Gupta <abg71297@gmail.com>
2023-05-30 20:11:54 +0530

    update base path

--- react/vite.config.js
+++ react/vite.config.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/react/dist/",
+	base: "./",
 	plugins: [react(), basicSsl()],
 	resolve: {
 		alias: {
