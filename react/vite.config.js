c747b51e6f3fa905aea2ae72d4cbf06e850b3b2c
Abhikarsh Gupta <abg71297@gmail.com>
2023-05-30 19:24:09 +0530

    Update vite.config.js

--- react/vite.config.js
+++ react/vite.config.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/wildTrails/react/",
+	base: "/react/",
 	plugins: [react(), basicSsl()],
 	resolve: {
 		alias: {
